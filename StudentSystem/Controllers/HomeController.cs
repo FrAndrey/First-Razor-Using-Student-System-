853347b05bc597c855171d8305a9207c2f1fb3b4
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-09-27 18:33:28 -0400

    Add project files.

---
+++ StudentSystem/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StudentSystem.Models;
+
+namespace StudentSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+
+
+
+        public ViewResult Index()
+        {
+            Student testStudent = new Student("IT", "andrey@mail.ru", "Andrey", true, 4.05, "Paramonov", 301); 
+            return View(testStudent);
+
+        }
+      
+        public ViewResult ListStudent()
+        {
+            Student bob = new Student("IT", "bob@mail.ru", "Bob", true, 4.05, "Milasovich", 302);
+            Student fred = new Student("Mec", "fred@mail.ru", "Fred", false, 3.05, "Fra", 303);
+            Student josh = new Student("Hosp", "josh@mail.ru", "Josh", true, 3.55, "Bur", 304);
+            Student melissa = new Student("Cook", "melissa@mail.ru", "Melissa", false, 2.95, "Mak", 304);
+
+            List<Student> students = new List<Student>();
+            students.Add(bob);
+            students.Add(fred);
+            students.Add(josh);
+            students.Add(melissa);
+
+            return View(students);
+        }
+
+
+
+    }
+}
---
+++ StudentSystem/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSystem.Models
+{
+    public class Student
+    {
+        public string CourseCode { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public bool FullTime { get; set; }
+        public double Gpa { get; set; }
+        public string LastName { get; set; }
+        public int StudentId { get; set; }
+
+        public Student (string course,string email,string first, bool full,double gppaa, string lastname,int id)
+        {
+            CourseCode = course;
+            Email = email;
+            FirstName = first;
+            FullTime = full;
+            Gpa = gppaa;
+            LastName = lastname;
+            StudentId = id;
+        }
+    }
+}
